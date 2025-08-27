6d213999d8e7ecc9a3c3497fd2fec679420b9256
Federico Beltramino <frbeltra2@gmail.com>
2025-08-27 08:38:14 +0200

    agrego sendgrid

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,38 +1,36 @@
-const nodemailer = require('nodemailer');
+const sgMail = require("@sendgrid/mail");
 
-// Transporter con Gmail (puede cambiar según el proveedor)
-const transporter = nodemailer.createTransport({
-  service: 'SendGrid', 
-  auth: {
-    user: 'apikey', 
-    pass: process.env.SENDGRID_API_KEY,
-  },
-});
+// Configuro la API Key (guárdala en Railway como variable SENDGRID_API_KEY)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// Función para enviar correo
-const sendAppointmentEmail = async (to, subject, htmlContent) => {
-  const nombreNegocio = `"${process.env.NOMBRE_NEGOCIO}" <${process.env.EMAIL_NEGOCIO}>`;
-  const mailOptions = {
-    from: nombreNegocio,
-    to: to, // destinatario
-    subject,
-    html: htmlContent,
-  };
+const sendEmail = async (to, subject, html) => {
+  try {
+    const msg = {
+      to,
+      from: {
+        email: process.env.EMAIL_NEGOCIO, 
+        name: process.env.NOMBRE_NEGOCIO 
+      },
+      subject,
+      html,
+    };
 
-  await transporter.sendMail(mailOptions);
+    await sgMail.send(msg);
+    console.log("📧 Email enviado correctamente");
+  } catch (error) {
+    console.error("❌ Error al enviar email:", error.response?.body || error);
+    throw new Error("No se pudo enviar el correo");
+  }
 };
 
-// Función para enviar correo
-const sendOTPEmail = async (to, subject, htmlContent) => {
-  const nombreNegocio = `"${process.env.NOMBRE_NEGOCIO}" <${process.env.EMAIL_NEGOCIO}>`;
-  const mailOptions = {
-    from: nombreNegocio,
-    to: to,
-    subject,
-    html: htmlContent,
-  };
+// Enviar correo para turno
+const sendAppointmentEmail = async (to, subject, htmlContent) => {
+  await sendEmail(to, subject, htmlContent);
+};
 
-  await transporter.sendMail(mailOptions);
+// Enviar correo para OTP
+const sendOTPEmail = async (to, subject, htmlContent) => {
+  await sendEmail(to, subject, htmlContent);
 };
 
 module.exports = {
